7efab0bce6fce626823206d80f4aad6ad4edfd6b
Zinadine Muñoz Luna <zmunoz@outlook.es>
2025-11-12 08:59:00 -0800

    Made dev branch, add graphics window term

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,16 @@ int main()
     mainMenu.setMenuOption(GameUI::MenuOption::Options);
     mainMenu.displayCurrentOption();
 
-    
-    
+    terminal_open();
+
+    terminal_set("window: size=100x30");
+    terminal_print(2, 1, "Hello, World!");
+
+    terminal_refresh();
+    terminal_read();
+
+    terminal_close();
+
     // terminal_open();
 
     // terminal_set("window: size=80x25");
